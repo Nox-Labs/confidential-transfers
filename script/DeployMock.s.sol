0c39f8786d039566ea9ea1993a3f9d4f3a5e017a
Nox-admin <dev@noxlabs.xyz>
2025-12-24 10:54:39 +0300

    Update foundry.toml to add Sepolia RPC endpoint and modify DeployMock script to enhance logging of sender and token balances.

--- script/DeployMock.s.sol
+++ script/DeployMock.s.sol
@@ -24,6 +24,8 @@ contract DeployMock is Script {
             new MockERC20(20, initVerifier, applyVerifier, updateVerifier, transferVerifier, applyAndTransferVerifier);
         mockERC20.mint(msg.sender, 1e22);
         console.log("MockERC20 deployed to:", address(mockERC20));
-        console.log("Anvil 0 address:", msg.sender, "with balance:", mockERC20.balanceOf(msg.sender));
+        console.log("Sender aka Owner:", msg.sender);
+        console.log("Token Balance:", mockERC20.balanceOf(msg.sender));
+        console.log("Native Balance:", address(msg.sender).balance);
     }
 }
